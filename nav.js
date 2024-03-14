904843862461005a81472c5670581b7a369593f7
Ankit rai <122200322+Aniraai@users.noreply.github.com>
2024-03-14 17:19:49 +0530

    sdf

--- nav.js
+++ nav.js
@@ -14,3 +14,35 @@ hamburger.addEventListener("click", (e) => {
     hamburger.style.backgroundColor = "#65CFB4";
   }
 });
+const options = {
+  root: null,
+  rootMargin: "0px",
+  threshold: 0.5,
+};
+
+const handleIntersect = (entries, observer) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("visible");
+    } else {
+      entry.target.classList.remove("visible");
+    }
+  });
+};
+
+const observer = new IntersectionObserver(handleIntersect, options);
+
+const aboutHeading = document.querySelector(".about-us-content");
+const eventgrid = document.querySelector(".event_grid");
+const discordimg = document.querySelector(".about-us-headding JoinDis");
+
+if (aboutHeading) {
+  observer.observe(aboutHeading);
+}
+if (eventgrid) {
+  observer.observe(eventgrid);
+}
+
+if (discordimg) {
+  observer.observe(discordimg);
+}
